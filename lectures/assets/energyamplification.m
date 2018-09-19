b86a2b8d9a151e60ff3394e7ce009c479169ab91
Navid Constantinou <navidcy@gmail.com>
2018-09-19 09:46:01 +0300

    minor polishing in energyamplification.m script

--- lectures/assets/energyamplification.m
+++ lectures/assets/energyamplification.m
@@ -1,17 +1,27 @@
 clear all;
 
-A = [1 10;
-     0 -1];
-A = [0 1;
-     1 0];
+% isnormal = 0;
+
+
+for isnormal = [0, 1]
+
+if isnormal
+    A = [0 1;
+         1 0];
+ THETA = [-pi/3, -pi/4, pi/4];
+ filenamesav = 'energyamplifnormal.png';
+else
+    A = [1 10;
+         0 -1];
+ THETA = [-atan(1/5), 0, pi/3];
+ filenamesav = 'energyamplifnonnormal.png';
+end
  
  figure(1);clf;
  
  T = linspace(0, 3, 101);
  dt = T(2)-T(1);
  
- THETA = [-atan(1/5), 0, pi/3];
- THETA = [-pi/3, -pi/4, pi/4];
  
  for ith=1:length(THETA)
      theta = THETA(ith);
@@ -38,8 +48,14 @@ A = [0 1;
      hold on;
  end
  
-h = legend([pl(1), pl(2), pl(3)], '$-\pi/3$', '$-\pi/4$', '$\pi/4$');
+if isnormal
+    h = legend([pl(1), pl(2), pl(3)], '$-\pi/3$', '$-\pi/4$', '$\pi/4$');
+else
+    h = legend([pl(1), pl(2), pl(3)], '$-\tan^{-1}(1/5)$', '$0$', '$\pi/3$');
+end
 set(h, 'fontsize', 20, 'interpreter','latex', 'location', 'southwest')
 title(h, '$\theta$', 'fontsize', 20, 'interpreter','latex')
 set(gcf,'Renderer','painters','Color','w');
-print(gcf, '-dpng', '-r200', 'energyamplifnormal.png'); 
+print(gcf, '-dpng', '-r200', filanemasav); 
+
+end
